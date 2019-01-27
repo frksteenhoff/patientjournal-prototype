5f4887ae8fa387c57561a12b8beae982c39637c9
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2019-01-27 12:05:35 +0100

    Aagain..

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/')); 
 const PORT = process.env.PORT || 3000;
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 console.log(process.env.PORT, " ", PORT);	
-app.set( 'PORT', PORT);
 
 // Start node server
 app.listen( PORT, function() {
-  console.log( 'Node server is running on port ${PORT}');
+  console.log( 'Node server is running on port ' + PORT);
   });
