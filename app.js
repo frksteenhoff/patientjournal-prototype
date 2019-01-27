7a8b0d2d096e8ac333522a77bacd44e5fff5e04d
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2019-01-27 21:37:29 +0100

    Added more paths

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var path = require('path');
 var app = express();
 let server = http.createServer(app);
 
+app.use(express.static(path.join(__dirname + ''))); 
+app.use(express.static(path.join(__dirname + '/assets'))); 
+app.use(express.static(path.join(__dirname + '/src'))); 
+app.use(express.static(path.join(__dirname + '/fonts'))); 
+app.use(express.static(path.join(__dirname + '/dist'))); 
 app.use(express.static(path.join(__dirname))); 
+app.use(favicon(__dirname + 'assets/favicon.ico'))
 
 app.get('/', function(req, res, next){
     res.sendFile(path.join(__dirname, 'index.html'));
