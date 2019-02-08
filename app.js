9899dd06816360b2c3bfdddcd73de2c1bdfc9c31
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2019-02-08 19:11:59 +0100

    Edited port for prod/dev

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 'use strict';
 
-var express = require('express');
+let express = require('express');
 var http = require('http');
 var path = require('path');
 var app = express();
-var server = http.createServer(app);
+let server = http.createServer(app);
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(path.join(__dirname + ''))); 
+app.use(express.static(path.join(__dirname + '/assets'))); 
+app.use(express.static(path.join(__dirname + '/src'))); 
+app.use(express.static(path.join(__dirname + '/fonts'))); 
+app.use(express.static(path.join(__dirname + '/dist'))); 
+
+app.get('/', function(req, res, next){
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(__dirname + '/')); 
-
-server.listen(3000, 'localhost');
-server.on('listening', function() {
-  console.log('Express server started on port %s at %s', server.address().port, server.address().address);
-});
+// Start node server
+app.listen( process.env.PORT || 3000, function() {
+  console.log( 'Node server is running');
+  });
